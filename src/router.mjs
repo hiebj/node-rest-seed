2c5d90f1120a1b610fcaac0d327fc1939028babf
Nick Scharold <nick.scharold@gmail.com>
2018-09-28 23:53:20 -0600

    add diagnostics

--- src/router.mjs
+++ src/router.mjs
@@ -1,15 +1,20 @@
 import express from 'express'
 import Grade from './models/Grade'
+import Diagnostics from './models/Diagnostics'
 import RESTController from './RESTController'
-import SummaryController  from './SummaryController'
+import SummaryController from './SummaryController'
 
 const router = express.Router()
-const GradeController = RESTController(Grade)
 
+const GradeController = RESTController(Grade)
 router.get('/grade/', GradeController.query)
 router.post('/grade/', GradeController.save)
 router.post('/grade/:_id/', GradeController.update)
 router.delete('/grade/:_id/', GradeController.delete)
+
+const DiagnosticsController = RESTController(Diagnostics)
+router.get('/diagnostics/', DiagnosticsController.query)
+router.post('/diagnostics/', DiagnosticsController.save)
 
 router.get('/summary/', SummaryController.get)
 
