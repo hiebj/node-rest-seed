5d9880eac1cc2ff21d45128cb68da1b07574bd0c
Jon Hieb <hiebj@live.com>
2018-09-29 01:07:51 -0600

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -2,12 +2,16 @@ import yargs from 'yargs'
 import dbConnect from './dbConnect'
 import server from './server'
 
+// accept cli params
 const { db = 'test', port = 8080, host = '0.0.0.0' } = yargs.argv
-
-console.log('-------- rest-node --------\n')
+console.log('-------- node-rest --------\n')
 console.log('valid args: --db, --host, --port\n')
-const connection = `mongodb://127.0.0.1/${db}`
-dbConnect(connection)
+
+// connect to mongodb
+const dbAddress = `mongodb://127.0.0.1/${db}`
+dbConnect(dbAddress)
 console.log(`mongoose connected to ${connection}`)
+
+// start the express server
 server.listen(port, host)
 console.log(`REST server listening on ${host}:${port}\n\n`)
